f41164f9f7fa365d6c8a23541f1151b088fc0313
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-20 16:18:13 +0700

    feat: add provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import CartContextProvider from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
