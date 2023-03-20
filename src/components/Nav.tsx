905222be8c9c01de302d0ad6a1c43eac83051922
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-20 23:26:08 +0700

    feat: tidy up style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,30 +3,55 @@ import Container from "./Container";
 
 export default function Nav() {
   return (
-    <nav className="bg-black text-white py-5">
+    <nav className="py-5">
       <Container>
         <div className="flex justify-between">
           <section>
-            <Link to={"/"}>ShopEZ</Link>
+            <Link className="text-2xl font-bold" to={"/"}>
+              SHOPEZ
+            </Link>
           </section>
           <section>
-            <Link to={"/cart"}>
+            <Link
+              className="border-2 space-x-2 py-2 px-6 rounded-md border-black"
+              to={"/cart"}
+            >
               <svg
+                className="inline-block"
+                height="20px"
+                version="1.1"
+                viewBox="0 0 20 20"
+                width="20px"
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-shopping-cart"
               >
-                <circle cx="9" cy="21" r="1"></circle>
-                <circle cx="20" cy="21" r="1"></circle>
-                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                <title />
+                <desc />
+                <defs />
+                <g
+                  fill="none"
+                  fillRule="evenodd"
+                  id="Page-1"
+                  stroke="none"
+                  strokeWidth="1"
+                >
+                  <g
+                    fill="#000000"
+                    id="Core"
+                    transform="translate(-212.000000, -422.000000)"
+                  >
+                    <g
+                      id="shopping-cart"
+                      transform="translate(212.000000, 422.000000)"
+                    >
+                      <path
+                        d="M6,16 C4.9,16 4,16.9 4,18 C4,19.1 4.9,20 6,20 C7.1,20 8,19.1 8,18 C8,16.9 7.1,16 6,16 L6,16 Z M0,0 L0,2 L2,2 L5.6,9.6 L4.2,12 C4.1,12.3 4,12.7 4,13 C4,14.1 4.9,15 6,15 L18,15 L18,13 L6.4,13 C6.3,13 6.2,12.9 6.2,12.8 L6.2,12.7 L7.1,11 L14.5,11 C15.3,11 15.9,10.6 16.2,10 L19.8,3.5 C20,3.3 20,3.2 20,3 C20,2.4 19.6,2 19,2 L4.2,2 L3.3,0 L0,0 L0,0 Z M16,16 C14.9,16 14,16.9 14,18 C14,19.1 14.9,20 16,20 C17.1,20 18,19.1 18,18 C18,16.9 17.1,16 16,16 L16,16 Z"
+                        id="Shape"
+                      />
+                    </g>
+                  </g>
+                </g>
               </svg>
+              <span>Cart (0)</span>
             </Link>
           </section>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,21 +33,30 @@ export default function Home() {
     <div>
       <Container>
         <h1 className="text-center py-5 text-3xl font-bold">Our Products</h1>
+        <div className="h-[2px] opacity-50 bg-black mb-10"></div>
         {loading ? (
           "loading"
         ) : (
-          <div className="flex flex-wrap justify-between gap-x-5 gap-y-10">
+          <div className="flex flex-wrap justify-between gap-x-1 gap-y-10">
             {products?.map((product) => {
               return (
-                <div key={product.id} className="w-3/12">
+                <div
+                  key={product.id}
+                  className="w-3/12 border-2 p-5 border-black rounded-md border-opacity-40"
+                >
                   <Link to={`/product/${product.id}`}>
                     <img src={product.images[0]} alt="" />
                   </Link>
-                  <div className="text-center">
-                    <h1 className="font-bold">{product.title}</h1>
-                    <p>{formatMoney(product.price)}</p>
-                    <button onClick={() => addToCart(product, product.id)}>
-                      Add
+                  <div className="text-center m-2 space-y-2">
+                    <h1 className="font-medium text-xl">{product.title}</h1>
+                    <p className="font-bold text-2xl">
+                      {formatMoney(product.price)}
+                    </p>
+                    <button
+                      className="rounded-md py-2 text-white bg-black px-6"
+                      onClick={() => addToCart(product, product.id)}
+                    >
+                      Buy Now
                     </button>
                   </div>
                 </div>
