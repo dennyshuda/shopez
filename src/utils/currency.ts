e5cd8cd072dd4f81750390674c24598ef10bfc56
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-21 10:51:45 +0700

    feat: add optional

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,3 +1,3 @@
-export function formatMoney(price: number) {
-  return price.toLocaleString("en-US", { style: "currency", currency: "USD" });
+export function formatMoney(price: number | undefined) {
+  return price?.toLocaleString("en-US", { style: "currency", currency: "USD" });
 }
