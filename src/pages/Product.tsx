b028adfb21dc6a2b0b37f4e21583a8fedbb4f1a0
Denny Setyawan Huda <dennshgit@gmail.com>
2023-03-21 14:33:23 +0700

    feat: adjust image size

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -19,10 +19,18 @@ export default function Product() {
         ) : (
           <div className="flex gap-5">
             <div className="w-1/2">
-              <img src={detailProduct?.image} alt={detailProduct?.title} />
+              <img
+                className="w-4/6"
+                src={detailProduct?.image}
+                alt={detailProduct?.title}
+              />
             </div>
             <div className="w-1/2 space-y-5">
-              <h1 className="text-3xl font-medium">{detailProduct?.title}</h1>
+              <h1 className="uppercase font-bold text-2xl opacity-75">
+                - {detailProduct?.category}
+              </h1>
+              <h1 className="text-6xl font-medium">{detailProduct?.title}</h1>
+              <p className="text-3xl">Rating {detailProduct?.rating.rate} ★</p>
               <p className="text-4xl font-bold">
                 {formatMoney(detailProduct?.price)}
               </p>
@@ -31,7 +39,7 @@ export default function Product() {
                 onClick={() => addToCart(detailProduct, detailProduct?.id)}
                 className="rounded-md py-2 text-white bg-black px-6"
               >
-                Buy Now
+                Add to Cart
               </button>
             </div>
           </div>
